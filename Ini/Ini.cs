07ae9884b2d43758e5631a5cecacd63f7b68f0ef
Lee Hide <leed_off@outlook.com>
2018-09-04 23:43:30 +0200

    added Ini namespace and class

---
+++ Ini/Ini.cs
@@ -0,0 +1,196 @@
+﻿/*
+ * Author: @Lion_Kor
+ * Version: 1.1.0.1
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lutil.Ini
+{
+    /// <summary>
+    /// Manages an INI file with multiple sections.
+    /// For INI files with one or no sections use SimpleIni class.
+    /// Keys and sections are case-sensitive.
+    /// </summary>
+    public partial class Ini
+    {
+        /// <summary>
+        /// The path of this ini file as defined by the constructor of this class.
+        /// </summary>
+        public string Path
+        {
+            get;
+            private set;
+        }
+
+        private Dictionary<string, Dictionary<string, string>> rawData
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Names of all sections in the ini file.
+        /// </summary>
+        public string[] Sections
+        {
+            get
+            {
+                return GetSections ();
+            }
+        }
+
+        /// <summary>
+        /// number of values in the ini (all sections)
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                int c = 0;
+                foreach (var section in rawData)
+                {
+                    foreach (var item in rawData[section.Key])
+                    {
+                        c += 1;
+                    }
+                }
+                return c;
+            }
+        }
+
+        /// <summary>
+        /// number of values in the specific section in the ini
+        /// </summary>
+        public int CountIn (string section)
+        {
+            int c = 0;
+            foreach (var item in rawData[section])
+            {
+                c += 1;
+            }
+            return c;
+        }
+
+        /// <summary>
+        /// Creates a new ini file or opens an existing one and loads the data into memory.
+        /// </summary>
+        /// <param name="path">Relative or absolute path of the ini file including extension.</param>
+        public Ini (string path)
+        {
+            Path = path;
+            rawData = new Dictionary<string, Dictionary<string, string>> ();
+            loadData ();
+        }
+
+        private void loadData ()
+        {
+            string activeSection = null;
+            if (!File.Exists (Path))
+            {
+                File.Create (Path);
+            }
+            foreach (var item in File.ReadLines (Path))
+            {
+                if (item.Length >= 3)
+                {
+                    if (item[0] == '[' && item[item.Length - 1] == ']')
+                    {
+                        var tmp = item.Trim (new char[] { '[', ']' });
+                        activeSection = tmp;
+                    }
+                    else
+                    {
+                        if (activeSection != null)
+                        {
+                            var s = item.Split ('=');
+                            writeToData (activeSection, s[0].Trim (), s[1].Trim ());
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Forces all data to be reloaded from the file. 
+        /// This is redundant unless the file is changed
+        /// in an unexpected way (for example by another 
+        /// program during runtime).
+        /// </summary>
+        public void ReloadData ()
+        {
+            rawData = new Dictionary<string, Dictionary<string, string>> ();
+            loadData ();
+        }
+
+        /// <summary>
+        /// Forces the data to be saved to the ini file.
+        /// This is to be used if "saveAll" is set to "false"
+        /// in any method.
+        /// </summary>
+        public void SaveData ()
+        {
+            writeToFile (Path);
+        }
+
+        /// <summary>
+        /// Forces the data to be saved to a different ini file.
+        /// This does not modify the Path field of this instance.
+        /// </summary>
+        /// <param name="path">path to write the data to, one time.</param>
+        public void SaveData (string path)
+        {
+            writeToFile (path);
+        }
+
+        private void writeToFile (string path)
+        {
+            string tmp = System.IO.Path.GetTempFileName ();
+            string backup = path + ".backup";
+            using (StreamWriter file = new StreamWriter (tmp))
+            {
+                foreach (var section in rawData)
+                {
+                    file.WriteLine ('[' + section.Key + ']');
+                    foreach (var item in rawData[section.Key])
+                    {
+                        file.WriteLine (item.Key + '=' + item.Value);
+                    }
+                    //file.WriteLine ("\n");
+                }
+            }
+            try
+            {
+                File.Replace (tmp, path, backup);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new Exception ($"backup file={backup}", e);
+            }
+            File.Delete (backup);
+        }
+
+
+        /// <summary>
+        /// Returns the data in a tree-view type manner. 
+        /// It is recommended to test this first.
+        /// </summary>
+        public override string ToString ()
+        {
+            string tree = "";
+            foreach (var section in rawData)
+            {
+                tree += $"{section.Key}\n";
+                foreach (var item in rawData[section.Key])
+                {
+                    tree += $"  {item.Key}:\n";
+                    tree += $"     {item.Value}\n";
+                }
+                tree += "\n";
+            }
+            return tree;
+        }
+    }
+}
---
+++ Ini/IniReadWrite.cs
@@ -0,0 +1,181 @@
+﻿/*
+ * Author: @Lion_Kor
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutil.Ini
+{
+    public partial class Ini // READ / WRITE METHODS
+    {
+        private void writeToData ( string section, string key, string value )
+        {
+            if (!rawData.ContainsKey (section))
+            {
+                rawData.Add (section, new Dictionary<string, string> ());
+            }
+            rawData[section].Add (key, value);
+        }
+        /// <summary>
+        /// Looks up the specified key in the specified section and returns the corresponding value.
+        /// </summary>
+        /// <param name="section">Section in which the key and value are held.</param>
+        /// <param name="key">Key which holds the value.</param>
+        /// <returns>Value held by the key in the section. If section or key does not exist returns <see langword="null"/></returns>
+        public string Get ( string section, string key )
+        {
+            var Section = section.Trim ();
+            var Key = key.Trim ();
+
+            if (rawData.ContainsKey (Section))
+            {
+                if (rawData[Section].ContainsKey (Key))
+                {
+                    return rawData[Section][Key];
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Returns all keys within the section.
+        /// </summary>
+        public string[] GetAll ( string section )
+        {
+            List<string> Keys = new List<string> ();
+            var Section = section.Trim ();
+            if (rawData.ContainsKey (Section))
+            {
+                foreach (var key in rawData[Section])
+                {
+                    Keys.Add (key.Key);
+                }
+                return Keys.ToArray ();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get method for comma-seperated-values. This reads the value from the key in the section, 
+        /// splits it by comma and returns the values in an array. 
+        /// </summary>
+        /// <param name="section">Section in which the key and value are held.</param>
+        /// <param name="key">Key which holds the value.</param>
+        /// <returns>Values held by the key in the section. If section or key does not exist returns <see langword="null"/></returns>
+        public string[] GetCSV ( string section, string key )
+        {
+            // TODO implement AddCSV or similar
+            var Section = section.Trim ();
+            var Key = key.Trim ();
+
+            if (rawData.ContainsKey (Section))
+            {
+                if (rawData[Section].ContainsKey (Key))
+                {
+                    var data = rawData[Section][Key];
+                    return data.Split (',');
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// Returns a string[] containing all of the section labels in the ini.
+        /// </summary>
+        public string[] GetSections ()
+        {
+            return rawData.Keys.ToArray<string> ();
+        }
+        /// <summary>
+        /// Sets a value to a key in a section.
+        /// </summary>
+        /// <param name="section">Section containing the key</param>
+        /// <param name="key">Existing key or new key in the section, holding the value</param>
+        /// <param name="value">Changed value or new value the key should hold</param>
+        /// <param name="saveAll">Whether the data should be saved to the file after the operation. 
+        /// If this is set to <see langword="false"/> it is encouraged to call Ini.SaveData() shortly after this in 
+        /// order to prevent data loss. Not saving the data to the file before this instance is deleted
+        /// will result in loss of any changes. Default: <see langword="true"/></param>
+        /// <param name="allowNewSection">Whether a new section is allowed to be created if it does not already exist.
+        /// It is encouraged that Ini.Add(...) is used when adding, however setting this parameter to <see langword="true"/> 
+        /// will have the same effect.</param>
+        public void Set ( string section, string key, string value, bool saveAll = true, bool allowNewSection = false )
+        {
+            if (allowNewSection)
+            {
+                if (!rawData.ContainsKey (section))
+                {
+                    rawData.Add (section, new Dictionary<string, string> ());
+                    rawData[section].Add (key, value);
+                }
+                else
+                {
+                    rawData[section][key] = value;
+                }
+            }
+            else
+            {
+                if (rawData.ContainsKey (section))
+                {
+                    rawData[section][key] = value;
+                }
+            }
+            if (saveAll)
+            {
+                SaveData ();
+            }
+        }
+        /// <summary>
+        /// Adds a value to a key in a section. Adds a new key and section if either does not yet exist.
+        /// </summary>
+        /// <param name="section">Section containing the key</param>
+        /// <param name="key">Existing key or new key in the section, holding the value</param>
+        /// <param name="value">Changed value or new value the key should hold</param>
+        /// <param name="saveAll">Whether the data should be saved to the file after the operation. 
+        /// If this is set to <see langword="false"/> it is encouraged to call Ini.SaveData() shortly after this in 
+        /// order to prevent data loss. Not saving the data to the file before this instance is deleted
+        /// will result in loss of any changes. Default: <see langword="true"/></param>
+        /// <param name="allowNewSection">Whether a new section is allowed to be created if it does not already exist.
+        /// It is encouraged that Ini.Set(...) is used to modify values in existing sections.</param>
+        public void Add ( string section, string key, string value, bool saveAll = true, bool allowNewSection = true )
+        {
+            Set (section, key, value, saveAll, allowNewSection);
+        }
+        /// <summary>
+        /// Removes a key and its value in a specific section.
+        /// </summary>
+        /// <param name="section">Section containing the key</param>
+        /// <param name="key">Key that is to be removed</param>
+        /// <param name="saveAll">Whether the data should be saved to the file after the operation. 
+        /// If this is set to <see langword="false"/> it is encouraged to call Ini.SaveData() shortly after this in 
+        /// order to prevent data loss. Not saving the data to the file before this instance is deleted
+        /// will result in loss of any changes. Default: <see langword="true"/></param>
+        public void RemoveKey ( string section, string key, bool saveAll = true )
+        {
+            rawData[section].Remove (key);
+            if (saveAll)
+            {
+                SaveData ();
+            }
+        }
+        /// <summary>
+        /// Deletes a section and all the keys and corresponding values inside. Use with caution.
+        /// </summary>
+        /// <param name="section">Section to be deleted</param>
+        /// <param name="saveAll">Whether the data should be saved to the file after the operation. 
+        /// If this is set to <see langword="false"/> it is encouraged to call Ini.SaveData() shortly after this in 
+        /// order to prevent data loss. Not saving the data to the file before this instance is deleted
+        /// will result in loss of any changes. Default: <see langword="true"/></param>
+        public void RemoveSection ( string section, bool saveAll = true )
+        {
+            rawData.Remove (section);
+            if (saveAll)
+            {
+                SaveData ();
+            }
+        }
+    }
+}
