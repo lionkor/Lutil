381d8e35a6628c8720ceb979f0a67dd5372074b2
Lee Hide <leed_off@outlook.com>
2018-09-04 23:43:48 +0200

    added SimpleIni namespace and class

---
+++ SimpleIni/SimpleIni.cs
@@ -0,0 +1,155 @@
+﻿/*
+ * Author: @Lion_Kor
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lutil.SimpleIni
+{
+    /// <summary>
+    /// Manages an INI file with one or no sections.
+    /// For an INI file with multiple sections use Ini class.
+    /// Keys and sections are case-sensitive.
+    /// </summary>
+    public partial class SimpleIni
+    {
+        /// <summary>
+        /// The path of this ini file as defined by the constructor of this class.
+        /// </summary>
+        public string Path
+        {
+            get;
+            private set;
+        }
+
+        private Dictionary<string, string> rawData
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// The section this ini reads from and writes to. This is either the label of the section or an empty string.
+        /// </summary>
+        public string Section
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// number of values in the ini
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return rawData.Count;
+            }
+        }
+
+        /// <summary>
+        /// Opens the given file or creates a new one and loads the data into memory.
+        /// </summary>
+        /// <param name="path">The relative or absolute path of the ini file with extension.</param>
+        public SimpleIni (string path)
+        {
+            Path = path;
+            rawData = new Dictionary<string, string> ();
+            Section = "";
+            loadData ();
+        }
+        /// <summary>
+        /// Opens the given file or creates a new one and loads the data into memory.
+        /// SimpleIni only provides support for a maximum of one section, which can be named
+        /// in this constructor.
+        /// </summary>
+        /// <param name="path">Relative or absolute path of the ini file with extension.</param>
+        /// <param name="section">Section name</param>
+        public SimpleIni (string path, string section)
+        {
+            Path = path;
+            rawData = new Dictionary<string, string> ();
+            Section = $"[{section}]\n";
+            loadData ();
+        }
+
+        private void loadData ()
+        {
+            if (!File.Exists (Path))
+            {
+                File.WriteAllText (Path, "");
+            }
+
+            foreach (var item in File.ReadLines (Path))
+            {
+                if (item.Length >= 3)
+                {
+                    if (item[0] != '[')
+                    {
+                        var s = item.Split ('=');
+                        Set (s[0].Trim (), s[1].Trim (), false);
+                    }
+                    else
+                    {
+                        var s = item.Trim (new char[] { '[', ']' });
+                        Section = $"[{s}]\n";
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Forces all data to be reloaded from the file. 
+        /// This is redundant unless the file is changed
+        /// in an unexpected way (for example by another 
+        /// program during runtime).
+        /// </summary>
+        public void ReloadData ()
+        {
+            rawData = new Dictionary<string, string> ();
+            loadData ();
+        }
+        /// <summary>
+        /// Forces the data to be saved to the ini file.
+        /// This is to be used if "saveAll" is set to "false"
+        /// in any method.
+        /// </summary>
+        public void SaveData ()
+        {
+            writeToFile (Path);
+        }
+        /// <summary>
+        /// Forces the data to be saved to a different ini file.
+        /// This does not modify the Path field of this instance.
+        /// </summary>
+        /// <param name="path">path to write the data to, one time.</param>
+        public void SaveData (string path)
+        {
+            writeToFile (path);
+        }
+
+        private void writeToFile (string path)
+        {
+            string tmp = System.IO.Path.GetTempFileName ();
+            string backup = path + ".backup";
+            using (StreamWriter file = new StreamWriter (tmp))
+            {
+                file.Write (Section);
+                foreach (var item in rawData)
+                {
+                    file.WriteLine (item.Key + '=' + item.Value);
+                }
+            }
+            try
+            {
+                File.Replace (tmp, path, backup);
+            }
+            catch (Exception e)
+            {
+                throw new Exception ($"backup file={backup}", e);
+            }
+            File.Delete (backup);
+        }
+    }
+}
---
+++ SimpleIni/SimpleIniReadWrite.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Author: @Lion_Kor
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutil.SimpleIni
+{
+    public partial class SimpleIni
+    {
+        /// <summary>
+        /// Looks up the specified key and returns the corresponding value.
+        /// </summary>
+        /// <param name="key">Key which holds the value.</param>
+        /// <returns>Value held by the key.</returns>
+        public string Get (string key)
+        {
+            var Key = key.Trim ();
+            return rawData[Key];
+        }
+        /// <summary>
+        /// Sets a value to a key in a section.
+        /// </summary>
+        /// <param name="key">Existing key or new key, holding the value</param>
+        /// <param name="value">Changed value or new value the key should hold</param>
+        /// <param name="saveAll">Whether the data should be saved to the file after the operation. 
+        /// If this is set to <see langword="false"/> it is encouraged to call Ini.SaveData() shortly after this in 
+        /// order to prevent data loss. Not saving the data to the file before this instance is deleted
+        /// will result in loss of any changes. Default: <see langword="true"/></param>
+        public void Set (string key, string value, bool saveAll = true)
+        {
+            if (rawData.ContainsKey (key))
+            {
+                rawData[key] = value;
+            }
+            else
+            {
+                rawData.Add (key, value);
+            }
+
+            if (saveAll)
+            {
+                SaveData ();
+            }
+        }
+        /// <summary>
+        /// Adds a value to a key in a section. Adds a new key and section if either does not yet exist.
+        /// </summary>
+        /// <param name="key">Existing key or new key in the section, holding the value</param>
+        /// <param name="value">Changed value or new value the key should hold</param>
+        /// <param name="saveAll">Whether the data should be saved to the file after the operation. 
+        /// If this is set to <see langword="false"/> it is encouraged to call <see cref="SimpleIni.SaveData()"/> shortly after this in 
+        /// order to prevent data loss. Not saving the data to the file before this instance is deleted
+        /// will result in loss of any changes. Default: <see langword="true"/></param>
+        public void Add (string key, string value, bool saveAll = true)
+        {
+            Set (key, value, saveAll);
+        }
+        /// <summary>
+        /// Removes a key and its value.
+        /// </summary>
+        /// <param name="key">Key that is to be removed</param>
+        /// <param name="saveAll">Whether the data should be saved to the file after the operation. 
+        /// If this is set to <see langword="false"/> it is encouraged to call <see cref="SimpleIni.SaveData()"/> shortly after this in 
+        /// order to prevent data loss. Not saving the data to the file before this instance is deleted
+        /// will result in loss of any changes. Default: <see langword="true"/></param>
+        public void Remove (string key, bool saveAll = true)
+        {
+            rawData.Remove (key);
+            if (saveAll)
+            {
+                SaveData ();
+            }
+        }
+    }
+}
